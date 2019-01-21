56e081cb452b13020af499cca563f5db41deb503
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-21 22:13:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+let index = 0
+
 function init() {
-  // your code here
+  var index = 0
+    document.body.addEventListener('keydown', onKeyDownHandler);
+
+     function onKeyDownHandler(e) {
+      const key = parseInt(e.detail || e.which)
+      console.log('key', key)
+      if (key === code[index]) {
+        index++
+
+         if (index === code.length) {
+          alert("YOU DID IT!")
+          index = 0
+        }
+      } else {
+        index = 0
+      }
+    }
 }
+
+init()
